6c2d7a15fec9e6951dff780533c04818e43c333b
Alexhwa <ah.alswad@gmail.com>
2020-10-09 22:46:47 -0500

    Set up player script

---
+++ House of Horrors/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 moveVect;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVect = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
+    }
+}
