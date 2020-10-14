b9386ef4081da72190a2a2fe9f0e2fc69759f2b1
Alexhwa <ah.alswad@gmail.com>
2020-10-14 12:36:29 -0500

    Blob

---
+++ House of Horrors/Assets/Scripts/Layerer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Layerer : MonoBehaviour
+{
+    private Transform[] needsLayering;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Monster[] monsters = GameObject.FindObjectsOfType<Monster>();
+        needsLayering = new Transform[monsters.Length + 1];
+        for (int i = 0; i < monsters.Length; i++) {
+            needsLayering[i] = monsters[i].transform;
+        }
+        
+        needsLayering[monsters.Length] = GameObject.FindObjectOfType<PlayerController>().transform;
+        Comparer compare = new Comparer();
+        Array.Sort(needsLayering, compare);
+
+        print(StringFrmArray(needsLayering));
+    }
+    private string StringFrmArray(Transform[] array)
+    {
+        string result = "";
+        foreach (Transform e in array) {
+            result += e.name;
+        }
+        return result;
+    }
+    public class Comparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return ((Transform)x).position.y < ((Transform)y).position.y ? 1 : -1;
+        }
+    }
+}
---
+++ House of Horrors/Assets/Scripts/Monster.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public float moveSpeed;
+    public float runAwaySpeed;
+    public float minScareDist;
+
+    private Rigidbody2D rb;
+    private GameObject scaryObj;
+    private GameObject scaredExclamPnt;
+
+    public enum MoveDir
+    {
+        left = -1, right = 1
+    }
+    public enum MonsterState
+    {
+        Walking, Scared
+    }
+    public MoveDir moveDir;
+    public MonsterState state = MonsterState.Walking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        scaryObj = GameObject.FindGameObjectWithTag("Player");
+        scaredExclamPnt = transform.GetChild(0).gameObject;
+        scaredExclamPnt.SetActive(false);
+    }
+    private void Update()
+    {
+        if(Vector2.Distance(scaryObj.transform.position, transform.position) < minScareDist)
+        {
+            state = MonsterState.Scared;
+            scaredExclamPnt.SetActive(true);
+        }
+        else
+        {
+            state = MonsterState.Walking;
+            scaredExclamPnt.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MonsterAI();
+    }
+    public void MonsterAI()
+    {
+        if (state == MonsterState.Walking)
+        {
+            rb.velocity = new Vector2(moveSpeed * (float)moveDir, 0);
+        }
+        else if(state == MonsterState.Scared)
+        {
+            rb.velocity = Vector3.Normalize(transform.position - scaryObj.transform.position) * runAwaySpeed;
+
+        }
+    }
+}
