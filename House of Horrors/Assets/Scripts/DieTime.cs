cb42b109810e9188325baa300f2a50c32f78939c
Alexhwa <ah.alswad@gmail.com>
2020-10-18 01:43:44 -0500

    Gameplay

---
+++ House of Horrors/Assets/Scripts/DieTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieTime : MonoBehaviour
+{
+    public float dieTime;
+
+    void Start()
+    {
+        StartCoroutine(Die(dieTime));
+    }
+    IEnumerator Die(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+}
